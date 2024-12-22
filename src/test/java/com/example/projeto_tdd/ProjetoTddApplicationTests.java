595d9d1b6af8a50ba26889242b49c2c32a3ca633
matheus <matheusrodrig16@gmail.com>
2024-12-22 17:23:54 -0300

    first commit

---
+++ src/test/java/com/example/projeto_tdd/ProjetoTddApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.projeto_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+}
+
+/**
+ * 
+ * Criar uma api de vendas
+ * venda >= 1000.00 == 15%
+ * venda <= 1000.00 == 10%
+ * 
+ * classe comissão q tenha método calcular() q retorna o valor da comissão do tipo Double
+ */
